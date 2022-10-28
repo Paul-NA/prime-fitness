14b57105496b9399d6df31a337ed4c9260f7601a
Trugord <1210608+MMO-Indie@users.noreply.github.com>
2022-10-28 13:40:20 +0200

    correction du path

--- Application/Views/Ajax/searchListStructure.php
+++ Application/Views/Ajax/searchListStructure.php
@@ -13,7 +13,7 @@
                 <li class="list-group-item d-flex justify-content-between align-items-start">
                     <img src="https://www.gravatar.com/avatar/' . md5('partner' . $structure->getStructureId()) . '?s=50&r=pg&d=robohash" width="50" height="50" />
                     <div class="ms-2 me-auto">
-                        <div class="fw-bold"><a href="' . URI_ROOT . '/partner/information/' . $structure->getStructureId() . '">' . $this->cleanHTML($structure->getStructureName()) . '</a></div>
+                        <div class="fw-bold"><a href="' . URI_ROOT . '/structure/information/' . $structure->getStructureId() . '">' . $this->cleanHTML($structure->getStructureName()) . '</a></div>
                         Addresse : ' . $this->cleanHTML($user->getUserAddress()) . ' 
                     </div>
                     <div class="row align-items-center p-3"><b>' . (($structure->getStructureActive()) ? '<span class="text-bg-success p-2 rounded-3">Actif</span>' : '<span class="text-bg-danger p-2 rounded-3">Inactif</span>') . '</b></div>
